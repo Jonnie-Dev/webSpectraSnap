fdc3977e25386c07abdc39bb5eb2cc7fab540ab4
Jonnie <jtech2096@gmail.com>
2023-08-12 20:29:12 +0100

    project init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import APIForm from "./components/APIForm";
+import "./App.css";
+
+const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
-  
-  
-      return <div>Test</div>
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit an url!");
+    }
+  };
+
+  return (
+    <div className="whole-page">
+      <h1>Build Your Own Screenshot! 📸</h1>
+
+      <APIForm
+        inputs={inputs}
+        handleChange={(e) =>
+          setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value.trim(),
+          }))
+        }
+        //   onSubmit={submitForm}
+      />
+      <br></br>
+    </div>
+  );
 }
 
-export default App
+export default App;
